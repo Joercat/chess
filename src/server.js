f2de94dc3cd33833cbe386a47ec72b8e455ae41c
Joe <167563398+Joercat@users.noreply.github.com>
2025-02-25 10:50:02 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,36 +1,49 @@
 const WebSocket = require('ws');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
+const crypto = require('crypto');
 
-const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.end(data);
-    });
-  }
-});
-
+const server = http.createServer();
 const wss = new WebSocket.Server({ server });
-const clients = new Map();
+
+const rooms = new Map();
 
 wss.on('connection', (ws) => {
-  const id = Date.now();
-  clients.set(ws, id);
-
-  ws.on('message', (message) => {
-    const data = JSON.parse(message);
-    clients.forEach((clientId, client) => {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify(data));
-      }
+    ws.on('message', (message) => {
+        const data = JSON.parse(message);
+        
+        switch(data.type) {
+            case 'create':
+                const roomId = crypto.randomBytes(3).toString('hex');
+                rooms.set(roomId, {
+                    host: ws,
+                    players: [ws]
+                });
+                ws.send(JSON.stringify({type: 'roomCreated', roomId}));
+                break;
+                
+            case 'join':
+                if (rooms.has(data.roomId)) {
+                    const room = rooms.get(data.roomId);
+                    room.players.push(ws);
+                    room.guest = ws;
+                    
+                    room.host.send(JSON.stringify({type: 'playerJoined'}));
+                    ws.send(JSON.stringify({type: 'joinedRoom'}));
+                }
+                break;
+                
+            case 'move':
+                if (rooms.has(data.roomId)) {
+                    const room = rooms.get(data.roomId);
+                    const opponent = ws === room.host ? room.guest : room.host;
+                    opponent.send(JSON.stringify({
+                        type: 'move',
+                        move: data.move
+                    }));
+                }
+                break;
+        }
     });
-  });
-
-  ws.on('close', () => {
-    clients.delete(ws);
-  });
 });
 
 server.listen(3000);
