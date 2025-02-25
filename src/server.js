26b190c504e0bceb2902369f1e47d28fc654845c
Joe <167563398+Joercat@users.noreply.github.com>
2025-02-25 09:12:25 -0500

    Create server.js

---
+++ src/server.js
@@ -0,0 +1,38 @@
+const WebSocket = require('ws');
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile('chess.html', (err, data) => {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.end(data);
+    });
+  }
+});
+
+const wss = new WebSocket.Server({ server });
+
+const clients = new Map();
+
+wss.on('connection', (ws) => {
+  const id = Date.now();
+  clients.set(ws, id);
+
+  ws.on('message', (message) => {
+    const data = JSON.parse(message);
+    
+    // Broadcast moves to opponent
+    clients.forEach((clientId, client) => {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    });
+  });
+
+  ws.on('close', () => {
+    clients.delete(ws);
+  });
+});
+
+server.listen(3000);
