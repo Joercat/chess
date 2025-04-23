201b4d04b892f05b132f39a6d5bb5baacb07cd47
Joe <167563398+Joercat@users.noreply.github.com>
2025-04-23 16:18:18 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,7 +14,9 @@ io.on('connection', (socket) => {
         rooms.set(roomCode, {
             players: [socket.id],
             game: new Chess(),
-            currentTurn: socket.id
+            currentTurn: socket.id,
+            moveHistory: [],
+            lastMove: null
         });
         socket.join(roomCode);
         socket.emit('roomCreated', roomCode);
@@ -28,25 +30,34 @@ io.on('connection', (socket) => {
             socket.emit('joinedRoom', roomCode);
             io.to(roomCode).emit('gameStart', {
                 white: room.players[0],
-                black: room.players[1]
+                black: room.players[1],
+                fen: room.game.fen(),
+                history: room.moveHistory
             });
         } else {
             socket.emit('roomError', 'Room not found or full');
         }
     });
 
-    socket.on('move', ({ from, to, roomCode }) => {
+    socket.on('move', ({ from, to, promotion, roomCode }) => {
         const room = rooms.get(roomCode);
         if (room && socket.id === room.currentTurn) {
             try {
-                const move = room.game.move({ from, to });
+                const moveConfig = { from, to };
+                if (promotion) moveConfig.promotion = promotion;
+                
+                const move = room.game.move(moveConfig);
                 if (move) {
+                    room.lastMove = move;
+                    room.moveHistory.push(move);
                     room.currentTurn = room.players.find(id => id !== socket.id);
+                    
                     io.to(roomCode).emit('moveMade', {
                         from,
                         to,
                         fen: room.game.fen(),
-                        move: move
+                        move: move,
+                        history: room.moveHistory
                     });
 
                     if (room.game.isGameOver()) {
@@ -54,7 +65,15 @@ io.on('connection', (socket) => {
                         if (room.game.isCheckmate()) {
                             gameResult = `${room.game.turn() === 'w' ? 'Black' : 'White'} wins by checkmate!`;
                         } else if (room.game.isDraw()) {
-                            gameResult = 'Game is a draw!';
+                            if (room.game.isStalemate()) {
+                                gameResult = 'Game drawn by stalemate';
+                            } else if (room.game.isThreefoldRepetition()) {
+                                gameResult = 'Game drawn by threefold repetition';
+                            } else if (room.game.isInsufficientMaterial()) {
+                                gameResult = 'Game drawn by insufficient material';
+                            } else {
+                                gameResult = 'Game drawn by fifty-move rule';
+                            }
                         }
                         io.to(roomCode).emit('gameOver', gameResult);
                     }
@@ -65,19 +84,36 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on('resign', (roomCode) => {
+        const room = rooms.get(roomCode);
+        if (room) {
+            const winner = room.players.find(id => id !== socket.id);
+            const winnerColor = room.players[0] === winner ? 'White' : 'Black';
+            io.to(roomCode).emit('gameOver', `${winnerColor} wins by resignation!`);
+        }
+    });
+
     socket.on('newGame', (roomCode) => {
         const room = rooms.get(roomCode);
         if (room) {
             room.game = new Chess();
             room.currentTurn = room.players[0];
-            io.to(roomCode).emit('gameReset', room.game.fen());
+            room.moveHistory = [];
+            room.lastMove = null;
+            io.to(roomCode).emit('gameReset', {
+                fen: room.game.fen(),
+                turn: room.players[0]
+            });
         }
     });
 
     socket.on('disconnect', () => {
         rooms.forEach((room, roomCode) => {
             if (room.players.includes(socket.id)) {
-                io.to(roomCode).emit('playerLeft');
+                io.to(roomCode).emit('playerLeft', {
+                    playerId: socket.id,
+                    message: 'Opponent disconnected'
+                });
                 rooms.delete(roomCode);
             }
         });
@@ -86,5 +122,5 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Chess server running on port ${PORT}`);
 });
