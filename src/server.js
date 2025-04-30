d750eada9ed6161e669351f2e7af745ee1400f48
Joe <167563398+Joercat@users.noreply.github.com>
2025-04-30 18:13:32 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,8 +14,7 @@ io.on('connection', (socket) => {
         rooms.set(roomCode, {
             players: [socket.id],
             game: new Chess(),
-            currentTurn: socket.id,
-            white: socket.id
+            currentTurn: socket.id
         });
         socket.join(roomCode);
         socket.emit('roomCreated', roomCode);
@@ -25,12 +24,11 @@ io.on('connection', (socket) => {
         const room = rooms.get(roomCode);
         if (room && room.players.length < 2) {
             room.players.push(socket.id);
-            room.black = socket.id;
             socket.join(roomCode);
             socket.emit('joinedRoom', roomCode);
             io.to(roomCode).emit('gameStart', {
-                white: room.white,
-                black: room.black
+                white: room.players[0],
+                black: room.players[1]
             });
         } else {
             socket.emit('roomError', 'Room not found or full');
@@ -41,7 +39,7 @@ io.on('connection', (socket) => {
         const room = rooms.get(roomCode);
         if (room && socket.id === room.currentTurn) {
             try {
-                const move = room.game.move({ from, to, promotion: 'q' });
+                const move = room.game.move({ from, to });
                 if (move) {
                     room.currentTurn = room.players.find(id => id !== socket.id);
                     io.to(roomCode).emit('moveMade', {
@@ -50,19 +48,12 @@ io.on('connection', (socket) => {
                         fen: room.game.fen(),
                         move: move
                     });
-
                     if (room.game.isGameOver()) {
                         let gameResult;
                         if (room.game.isCheckmate()) {
                             gameResult = `${room.game.turn() === 'w' ? 'Black' : 'White'} wins by checkmate!`;
                         } else if (room.game.isDraw()) {
                             gameResult = 'Game is a draw!';
-                        } else if (room.game.isStalemate()) {
-                            gameResult = 'Game is a draw by stalemate!';
-                        } else if (room.game.isThreefoldRepetition()) {
-                            gameResult = 'Game is a draw by threefold repetition!';
-                        } else if (room.game.isInsufficientMaterial()) {
-                            gameResult = 'Game is a draw by insufficient material!';
                         }
                         io.to(roomCode).emit('gameOver', gameResult);
                     }
@@ -73,19 +64,11 @@ io.on('connection', (socket) => {
         }
     });
 
-    socket.on('resign', (roomCode) => {
-        const room = rooms.get(roomCode);
-        if (room) {
-            const winner = socket.id === room.white ? 'Black' : 'White';
-            io.to(roomCode).emit('gameOver', `${winner} wins by resignation!`);
-        }
-    });
-
     socket.on('newGame', (roomCode) => {
         const room = rooms.get(roomCode);
         if (room) {
             room.game = new Chess();
-            room.currentTurn = room.white;
+            room.currentTurn = room.players[0];
             io.to(roomCode).emit('gameReset', room.game.fen());
         }
     });
